6a7bceb5ed8367071c923fddfd155ac0606a399f
archaudhari <81674825+archaudhari@users.noreply.github.com>
2023-01-26 21:59:14 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Import stylesheets
-import './style.css';
-
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+let numbers = [4, 2, 5, 1, 3];
+numbers.sort();
+console.log(numbers); // [1, 2, 3, 4, 5]
